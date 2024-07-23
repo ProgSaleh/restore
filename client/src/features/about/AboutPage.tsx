506f79998f25cc06ad9f4c96db0bbbb859c4b472
Progsaleh <progsaleh@hotmail.com>
2024-07-23 22:21:12 +0300

    end section 4

---
+++ client/src/features/about/AboutPage.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material';
+
+export default function AboutPage() {
+  return (
+    <Typography variant="h2">
+      <div>AboutPage</div>
+    </Typography>
+  );
+}
